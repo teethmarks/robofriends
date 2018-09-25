474e717e7844deb7a2ac833621d7df5787b69fde
Seah Liwei <teethmarks@gmail.com>
2018-09-25 23:13:56 +0800

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import Card from './Card';
+import CardList from './CardList';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import { robots } from './robots';
+
+ReactDOM.render(
+	<CardList robots={robots} />
+, document.getElementById('root'));
+registerServiceWorker();
