3d2c20f32c1a9e24bb61da0570f769dc827055cd
Seah Liwei <teethmarks@gmail.com>
2019-01-07 00:16:21 +0800

    Restarting this React course.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Card from './Card';
-import CardList from './CardList';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
+import Card from './Card';
 import { robots } from './robots';
+import 'tachyons';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-	<CardList robots={robots} />
+	<div>
+		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+	</div>
 , document.getElementById('root'));
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
